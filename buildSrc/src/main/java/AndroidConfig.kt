6c24a21228563501f0825d173ba746fd19502308
Lsy <1228357237@qq.com>
2023-09-18 14:52:20 +0800

    提现金额字段修改，投放器显示语音设置

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -11,7 +11,7 @@ object AndroidConfig {
         val applicationId = "com.yunshang.haileshenghuo"
         val minSdk = 21
         val targetSdk = 33
-        val versionCode = 219
-        val versionName = "2.1.3"
+        val versionCode = 220
+        val versionName = "2.1.4"
     }
 }

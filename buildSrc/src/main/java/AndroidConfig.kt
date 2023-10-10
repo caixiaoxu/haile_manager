5d0bc5dba5b4cc854d136ae646b5acf83c1948ee
Lsy <1228357237@qq.com>
2023-10-10 13:33:16 +0800

    2.1.11版本

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -11,7 +11,7 @@ object AndroidConfig {
         val applicationId = "com.yunshang.haileshenghuo"
         val minSdk = 21
         val targetSdk = 33
-        val versionCode = 226
-        val versionName = "2.1.10"
+        val versionCode = 227
+        val versionName = "2.1.11"
     }
 }

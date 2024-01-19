e2a9ded5aa5acad107fc92863536994de94d268c
Lsy <1228357237@qq.com>
2024-01-19 18:19:42 +0800

    合并代码

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -11,7 +11,7 @@ object AndroidConfig {
         val applicationId = "com.yunshang.haileshenghuo"
         val minSdk = 21
         val targetSdk = 33
-        val versionCode = 246
-        val versionName = "2.2.1"
+        val versionCode = 247
+        val versionName = "2.2.2"
     }
 }

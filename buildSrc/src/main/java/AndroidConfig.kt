1ea7549ee94b144064c09e87cff511eb663b8126
Lsy <1228357237@qq.com>
2024-01-25 13:36:37 +0800

    人员管理门店太多显示问题

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -11,7 +11,7 @@ object AndroidConfig {
         val applicationId = "com.yunshang.haileshenghuo"
         val minSdk = 21
         val targetSdk = 33
-        val versionCode = 249
-        val versionName = "2.2.4"
+        val versionCode = 250
+        val versionName = "2.2.5"
     }
 }
